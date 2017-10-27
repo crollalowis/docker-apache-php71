cf6d4c8f2c42a0e300b1b85d92e73ae0d1a7c6ab
Steven Agyekum <s.agyekum@crolla-lowis.de>
2017-10-27 14:19:00 +0200

    added entrypoint file

---
+++ bootstrap/entry.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+	
+cd /var/www/html/
+
+# Download Craft CMS by using Craft CLI
+# If Craft CMS exists already, the download will be skipped.
+
+echo "Downloading Craft CMS..."
+
+craft download --terms --no-prompt
+
+# Run Apache2
+
+apache2-foreground
+
